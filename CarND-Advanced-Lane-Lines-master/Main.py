8bd06d815291f35aeed2f51567b994383a4e351f
Ballmerpeakwarrior <pr.sn.777@gmail.com>
2020-09-21 19:10:05 -0400

    Testing

--- CarND-Advanced-Lane-Lines-master/Main.py
+++ CarND-Advanced-Lane-Lines-master/Main.py
@@ -2,8 +2,17 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import glob
 
-#ChessBoard
+#Camera Calibration
+
+img = mpimg.imread('camera_cal/calibration1.jpg')
+plt.imshow(img)
+
+
+#Number of corners on chessboard
 nx = 9
 ny = 6
 
+
+plt.show()
