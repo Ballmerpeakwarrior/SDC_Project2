7172d1029f1e96c0ea7e90d8371e383f5174d850
Ballmerpeakwarrior <pr.sn.777@gmail.com>
2020-09-23 20:53:33 -0400

    undistort, color transform, gradients

--- CarND-Advanced-Lane-Lines-master/Main.py
+++ CarND-Advanced-Lane-Lines-master/Main.py
@@ -17,7 +17,7 @@ objp[:,:2] = np.mgrid[0:nx,0:ny].T.reshape(-1,2) #Array with all the object poin
 
 for fname in CalibrationImages:
     #Import image
-    img = cv2.imread('camera_cal/calibration2.jpg')
+    img = cv2.imread(fname)
     plt.figure(1)
     plt.imshow(img)
 
@@ -47,7 +47,45 @@ undist = cv2.undistort(img, mtx, dist, None, mtx)
 plt.figure(3)
 plt.imshow(undist)
 
-##### ----- Compute the camera calibration matrix and distortion coefficients given a set of chessboard images ----- #####
+##### ----- Apply a distortion correction to raw images ----- #####
+TestImages = glob.glob('test_images/test*.jpg')
+for fname in TestImages:
+    #Import test image
+    img = cv2.imread(fname)
+
+    #Undistort test image
+    undist = cv2.undistort(img, mtx, dist, None, mtx)
+
+##### ----- Use color transforms, gradients, etc., to create a thresholded binary image ----- #####
+    #Grayscale & HLS Color Space
+    gray = cv2.cvtColor(undist, cv2.COLOR_BGR2GRAY)
+    hls = cv2.cvtColor(undist, cv2.COLOR_RGB2HLS)
+    S = hls[:,:,2]
+
+    # Sobel x
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0) # Take the derivative in x
+    abs_sobelx = np.absolute(sobelx) # Absolute x derivative to accentuate lines away from horizontal
+    scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
+
+    # Threshold x gradient
+    thresh_min = 20
+    thresh_max = 100
+    sxbinary = np.zeros_like(scaled_sobel)
+    sxbinary[(scaled_sobel >= thresh_min) & (scaled_sobel <= thresh_max)] = 1
+
+    # Threshold color channel
+    s_thresh_min = 170
+    s_thresh_max = 255
+    s_binary = np.zeros_like(S)
+    s_binary[(S >= s_thresh_min) & (S <= s_thresh_max)] = 1
+
+    combined_binary = np.zeros_like(sxbinary)
+    combined_binary[(s_binary == 1) | (sxbinary == 1)] = 1
+
+    plt.figure(4)
+    plt.imshow(combined_binary, cmap = 'gray')
+
+
 
 
 plt.show()
