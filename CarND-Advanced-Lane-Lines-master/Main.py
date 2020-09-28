d8ab17e872711aab25b97475518f7ea69ae5083e
Ballmerpeakwarrior <pr.sn.777@gmail.com>
2020-09-27 21:46:04 -0400

    PipelineDon

--- CarND-Advanced-Lane-Lines-master/Main.py
+++ CarND-Advanced-Lane-Lines-master/Main.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import glob
+from moviepy.editor import VideoFileClip
+from IPython.display import HTML
 
 ##### ----- Compute the camera calibration matrix and distortion coefficients given a set of chessboard images ----- #####
 
@@ -107,6 +109,7 @@ rightx_base = np.argmax(histogram[midpoint:]) + midpoint # Index of point in his
 nwindows = 9
 margin = 100
 minpix = 50
+out_img = np.dstack((warped, warped, warped))
 
 # Set height of windows - based on nwindows above and image shape
 window_height = np.int(warped.shape[0]//nwindows)
@@ -121,5 +124,89 @@ rightx_current = rightx_base
 # Create empty lists to receive left and right lane pixel indices
 left_lane_inds = []
 right_lane_inds = []
+
+for window in range(nwindows):
+    # Identify window boundaries in x and y (and right and left)
+    win_y_low = warped.shape[0] - (window+1)*window_height
+    win_y_high = warped.shape[0] - window*window_height
+    win_xleft_low = leftx_current - margin
+    win_xleft_high = leftx_current + margin
+    win_xright_low = rightx_current - margin
+    win_xright_high = rightx_current + margin
+    
+    # Draw the windows on the visualization image
+    cv2.rectangle(out_img,(win_xleft_low,win_y_low),
+    (win_xleft_high,win_y_high),(0,255,0), 2) 
+    cv2.rectangle(out_img,(win_xright_low,win_y_low),
+    (win_xright_high,win_y_high),(0,255,0), 2) 
+    
+    # Identify the nonzero pixels in x and y within the window #
+    good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & 
+    (nonzerox >= win_xleft_low) &  (nonzerox < win_xleft_high)).nonzero()[0]
+    good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & 
+    (nonzerox >= win_xright_low) &  (nonzerox < win_xright_high)).nonzero()[0]
+    
+    # Append these indices to the lists
+    left_lane_inds.append(good_left_inds)
+    right_lane_inds.append(good_right_inds)
+    
+    # If you found > minpix pixels, recenter next window on their mean position
+    if len(good_left_inds) > minpix:
+        leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+    if len(good_right_inds) > minpix:        
+        rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+# Concatenate the arrays of indices (previously was a list of lists of pixels)
+try:
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
+except ValueError:
+    # Avoids an error if the above is not implemented fully
+    pass
+
+# Extract left and right line pixel positions
+leftx = nonzerox[left_lane_inds]
+lefty = nonzeroy[left_lane_inds] 
+rightx = nonzerox[right_lane_inds]
+righty = nonzeroy[right_lane_inds]
+
+### TO-DO: Fit a second order polynomial to each using `np.polyfit` ###
+left_fit = np.polyfit(lefty, leftx, 2)
+right_fit = np.polyfit(righty, rightx, 2)
+
+# Generate x and y values for plotting
+ploty = np.linspace(0, warped.shape[0]-1, warped.shape[0] )
+try:
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+except TypeError:
+    # Avoids an error if `left` and `right_fit` are still none or incorrect
+    print('The function failed to fit a line!')
+    left_fitx = 1*ploty**2 + 1*ploty
+    right_fitx = 1*ploty**2 + 1*ploty
+
+## Visualization ##
+# Colors in the left and right lane regions
+out_img[lefty, leftx] = [255, 0, 0]
+out_img[righty, rightx] = [0, 0, 255]
+
+# Plots the left and right polynomials on the lane lines
+plt.figure(8)
+plt.plot(left_fitx, ploty, color='yellow')
+plt.plot(right_fitx, ploty, color='yellow')
+
+# Define y-value where we want radius of curvature
+# We'll choose the maximum y-value, corresponding to the bottom of the image
+y_eval = np.max(ploty)
+
+# Define conversions in x and y from pixels space to meters
+ym_per_pix = 30/720 # meters per pixel in y dimension
+xm_per_pix = 3.7/700
+
+left_curverad = ((1 + (2*left_fitx[0]*y_eval*ym_per_pix + left_fitx[1])**2)**1.5) / np.absolute(2*left_fitx[0])
+right_curverad = ((1 + (2*right_fitx[0]*y_eval*ym_per_pix + right_fitx[1])**2)**1.5) / np.absolute(2*right_fitx[0])
+print(left_curverad, right_curverad)
+
+plt.imshow(out_img)
 
 plt.show()
